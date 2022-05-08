c6c1ec94263a17cf38c1b4711e7ebef2edc12ea8
Frederico Zica <fredzica@gmail.com>
2022-05-08 13:14:49 -0300

    showing active nav link

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <header className="masthead mb-auto">
       <div className="inner">
         <nav className="nav nav-masthead justify-content-center">
-          <Link className="nav-link active" to="/">
+          <NavLink activeClassName="active" className="nav-link" to="/">
             Home
-          </Link>
-          <Link className="nav-link" to="/about">
+          </NavLink>
+          <NavLink activeClassName="active" className="nav-link" to="/about">
             About
-          </Link>
+          </NavLink>
         </nav>
       </div>
     </header>
