6098cf5766902923f861d61de9f48b38d02ede73
Frederico Zica <fredzica@gmail.com>
2022-05-08 08:24:36 -0300

    starting to use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="text-center">
+      <header>
+        <nav className="nav">
+          <a className="nav-link" href="#">
+            Home
+          </a>
+          <a className="nav-link" href="#">
+            About
+          </a>
+        </nav>
       </header>
+      <main>
+        <h1>Frederico Zica</h1>
+      </main>
+      <footer></footer>
     </div>
   );
 }
